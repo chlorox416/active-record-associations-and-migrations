eb0fb41274e9bab1edf765325fcbd50912e7cfa5
chlorox416 <chloechang416@gmail.com>
2020-09-24 16:47:28 -0400

    save

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,21 @@
 class Artist < ActiveRecord::Base
+    has_many :songs
+    has_many :genres, through: :songs
+
+    
+        def get_first_song
+          self.songs.first
+        end
+    
+        def get_genre_of_first_song
+            self.get_first_song.Genre
+        end
+        
+        # def song_count
+        #     self.songs.count {|song| song.all.}
+
+
+    
+
+
 end
